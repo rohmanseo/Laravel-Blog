8e5540776d85685b7cb9e42cc81d7cacf0d86f1b
Rohmanseo <rohmann311@gmail.com>
2019-10-15 13:41:34 +0700

    Mengubah judul blog

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Rohmanseo
                 </div>
 
                 <div class="links">
